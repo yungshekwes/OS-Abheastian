90d0d985fb7b56df13b257519e2cef34f4debbea
droneinspection <abhishek2aa2@gmail.com>
2023-03-24 19:03:53 +0100

    2a 2b 2c hopefully

--- Lab_4/exec.c
+++ Lab_4/exec.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "elf.h"
+#include <sys/mman.h>
+#include <string.h>
 
 
 struct elf_header read_elf_header(const char *elfFile){
@@ -44,12 +46,13 @@ struct elf_proghdr* read_prog_headers(const char* elfFile, struct elf_header hea
     
     // List of program headers
     struct elf_proghdr *progHeaders = malloc(count * sizeof(struct elf_proghdr));
-    
-    // Moving to the first program header location
-    for (int i = 0; i < count; i++) {
-        fseek(file, header.proghdr_offset+(i*header.proghdr_size), SEEK_SET);
-        fread(&progHeaders[i], sizeof(struct elf_proghdr), 1, file);
-    }
+
+    // Move to the first Program Header file location
+    fseek(file, header.proghdr_offset, SEEK_SET);
+
+    // Read all Program Headers into the array
+    fread(progHeaders,sizeof(struct elf_proghdr) * header.proghdr_count, 1,file);
+
     fclose(file);
     return progHeaders;
 }
@@ -57,11 +60,68 @@ struct elf_proghdr* read_prog_headers(const char* elfFile, struct elf_header hea
 int main(int argc, char *argv[]) {
     struct elf_header header = read_elf_header("helloworld_static");
     struct elf_proghdr *progHeaders = read_prog_headers("helloworld_static", header);
-    for (int i = 0; i < header.proghdr_count; i++){
-        printf("Prog header size of file: %ld for idx %d\n", progHeaders[i].size_file, i);
+
+    uint64_t totalSpan = 0;
+    // iterate through array of program headers
+    for (int i = 0; i < header.proghdr_count; i++) {
+        if (progHeaders[i].type == ELF_PROG_LOAD) {
+            // These are the Program Headers that are of type 'LOAD'
+            printf("%d\n", i);
+
+            // Part 2a
+            // Determine the total span of all loadable segments
+            totalSpan += progHeaders[i].size_memory;
+        }
     }
-    // printf("Main header size = %d\nProgram header size = %d\nSection header size = %d\nProgram header count = %d", 
-    // header.header_size, header.proghdr_size, header.secthdr_size, header.proghdr_count);
+
+    // is this correct?
+    // Part 2a
+    // Allocating memory using mmap()
+    void *base = mmap(NULL, totalSpan, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (base == MAP_FAILED) {
+        perror("mmap");
+        exit(1);
+    }
+
+    // Part 2b
+    // Fill allocated memory with binary contents, taking into account all offsets to preserve structure
+    off_t offset = 0;
+    for (int i = 0; i < header.proghdr_count; i++) {
+        if (progHeaders[i].type == ELF_PROG_LOAD) {
+            memcpy(base + offset, (void *)header.entry + progHeaders[i].file_offset, progHeaders[i].size_file);
+            offset += progHeaders[i].size_memory;
+        }
+    }
+
+    // Part 2c
+    // Protect memory regions with correct flags from the program header
+    offset = 0;
+    for (int i = 0; i < header.proghdr_count; i++) {
+        if (progHeaders[i].type == ELF_PROG_LOAD) {
+            int protect = 0;
+            if (progHeaders[i].flags & ELF_PROG_FLAG_READ) {
+                // |= is the bitwise OR operator!
+                protect |= PROT_READ;
+            }
+            if (progHeaders[i].flags & ELF_PROG_FLAG_WRITE) {
+                // |= is the bitwise OR operator!
+                protect |= PROT_WRITE;
+            }
+            if (progHeaders[i].flags & ELF_PROG_FLAG_EXEC) {
+                // |= is the bitwise OR operator!
+                protect |= PROT_EXEC;
+            }
+            mprotect(base + offset, progHeaders[i].size_memory, protect);
+            offset += progHeaders[i].size_memory;
+        }  
+    }
+
+    // Freeing allocated mmap memory
+    if (munmap(base, totalSpan) == -1) {
+        perror("munmap");
+        exit(1);
+    }
+
     free(progHeaders);
     return 0;
 }
